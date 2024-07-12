1a8670a2840e49ff09414df44874dfb48b2c4d03
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-12 10:51:31 +0200

    Update chapters

--- src/app.rs
+++ src/app.rs
@@ -66,20 +66,16 @@ fn get_player_position(app: &mut App) -> Vec2 {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_testing() {
-        assert_eq!(1 + 1, 2)
-    }
-
-    #[test]
-    fn test_can_create_app() {
-        create_app();
-    }
 
     #[test]
     fn test_empty_app_has_no_players() {
         let mut app = App::new();
         assert_eq!(count_n_players(&mut app), 0);
+    }
+
+    #[test]
+    fn test_can_create_app() {
+        create_app();
     }
 
     #[test]
